027df0c509774d3cdd7a11d713b2ce9168588208
Ramzi <ramzibenmansour.pro@gmail.com>
2025-09-14 16:14:05 +0200

    feat: Add User entity, enable typeORM synchronize and create initial table in database

--- src/domain/entity/user.entity.ts
+++ src/domain/entity/user.entity.ts
@@ -1,8 +1,33 @@
-import { Entity } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column('varchar', { length: 255, unique: true })
   login: string;
+
+  @Column('varchar', { length: 255, nullable: false })
   password: string;
+
+  @ApiProperty({ type: () => Date })
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @ApiProperty({ type: () => Date })
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+
+  @ApiProperty({ type: () => Date })
+  @DeleteDateColumn({ type: 'timestamptz' })
+  deletedAt: Date | null;
 }
--- src/infrastructure/app-data-source.ts
+++ src/infrastructure/app-data-source.ts
@@ -9,4 +9,5 @@ export const appDataSource: DataSource = new DataSource({
   username: process.env.PG_USERNAME,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
+  synchronize: true,
 });

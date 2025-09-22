ff0e09e5a231bf5b8badc5744a486d70e78ee5ea
Ramzi <ramzibenmansour.pro@gmail.com>
2025-09-23 00:10:29 +0200

    feat(accessibility): enforce unique feature names in create and update

--- src/application/service/accessibility-feature.app.service.ts
+++ src/application/service/accessibility-feature.app.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 import { AccessibilityFeature } from '../../domain/entity/accessibility-feature.entity';
+import { AccessibilityFeatureNotFoundException } from '../../domain/exception/accessibility/accessibility-feature-not-found.exception';
 import { AccessibilityFeatureRepository } from '../../infrastructure/repository/accessibility-feature.repository';
 import { ListBuilder, ListInterface } from '../common/list';
 import { AccessibilityFeatureCreateRequestDto } from '../dto/accessibility/accessibility-feature-create-request.dto';
+import { AccessibilityFeatureNameTakenException } from '../dto/accessibility/accessibility-feature-name-taken.exception';
 import { AccessibilityFeatureUpdateRequestDto } from '../dto/accessibility/accessibility-feature-update-request.dto';
 
 @Injectable()
@@ -15,6 +17,15 @@ export class AccessibilityFeatureAppService {
   async create(
     dto: AccessibilityFeatureCreateRequestDto,
   ): Promise<AccessibilityFeature> {
+    try {
+      await this.featureRepository.getByName(dto.name);
+      throw new AccessibilityFeatureNameTakenException(dto.name);
+    } catch (e) {
+      if (!(e instanceof AccessibilityFeatureNotFoundException)) {
+        throw e;
+      }
+    }
+
     const feature = new AccessibilityFeature();
 
     feature.name = dto.name;
@@ -31,7 +42,19 @@ export class AccessibilityFeatureAppService {
   ): Promise<AccessibilityFeature> {
     const feature = await this.featureRepository.getOneById(id);
 
-    feature.name = dto.name ?? feature.name;
+    if (dto.name && dto.name !== feature.name) {
+      try {
+        await this.featureRepository.getByName(dto.name);
+
+        throw new AccessibilityFeatureNameTakenException(dto.name);
+      } catch (e) {
+        if (!(e instanceof AccessibilityFeatureNotFoundException)) {
+          throw e;
+        }
+      }
+      feature.name = dto.name;
+    }
+
     feature.description = dto.description ?? feature.description;
     feature.icon = dto.icon ?? feature.icon;
     feature.category = dto.category ?? feature.category;
